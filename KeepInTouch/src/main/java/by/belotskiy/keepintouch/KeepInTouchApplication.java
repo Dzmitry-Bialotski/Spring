f174231382a3c890eda15043689387b2b92e3397
Dmitriy Belotskiy <dimasiandro@yandex.by>
2021-08-20 09:55:18 +0300

    init KeepInTouch app (backend)

---
+++ KeepInTouch/src/main/java/by/belotskiy/keepintouch/KeepInTouchApplication.java
@@ -0,0 +1,13 @@
+package by.belotskiy.keepintouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepInTouchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepInTouchApplication.class, args);
+    }
+
+}
